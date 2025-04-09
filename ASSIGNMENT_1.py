6df416af979e1f57ce3fc8c53ba9486d47892766
SURAJ-GHOST <suraj.programs@gmail.com>
2025-04-10 03:43:29 +0530

    Add files via upload
    ASSIGNMENT 1:
    Module 2: Basic Python Concepts
     
    Task 1: Perform Basic Mathematical Operations
    Problem Statement: Write a Python program that does the following:
    1.  Takes two numbers as input from the user.
    2.  Performs the basic mathematical operations on these two numbers:
    o	Addition
    o	Subtraction
    o	Multiplication
    o	Division
    3.  Displays the results of each operation on the screen.
    
    Task 2: Create a Personalized Greeting
    Problem Statement: Write a Python program that:
    1.  Takes a user's first name and last name as input.
    2.  Concatenates the first name and last name into a full name.
    3.  Prints a personalized greeting message using the full name.

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,34 @@
+#Task 1: Perform Basic Mathematical Operations
+#Problem Statement: Write a Python program that does the following:
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (division by zero is not allowed)"
+
+print("\nResults:")
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print("\n")
+
+#Task 2: Create a Personalized Greeting
+#Problem Statement: Write a Python program that:
+
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print(f"You are {full_name} the faous warlock and exorcist")
